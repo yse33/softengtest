2378e653eb6e7250329d693c349e75374c25758a
Yasen Angelov <yse33@abv.bg>
2024-03-21 21:35:39 +0200

    viktor commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+def even_numbers(n):
+    return [x for x in range(n) if x % 2 == 0]
+
+
+def print_menu():
+    print("2. Even numbers")
+
+
+def main():
+    print_menu()
+    function = int(input("Your choice: "))
+    maxNum = int(input("Enter max number: "))
+    if function == 2:
+        print(str(even_numbers(maxNum))[1:-1])
+    else:
+        print("Invalid choice")
+
+
+if __name__ == '__main__':
+    main()
 
