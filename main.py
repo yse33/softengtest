0d38311998c231b4d53889071f30cb78871c2d88
Yasen Angelov <yse33@abv.bg>
2024-03-21 21:34:51 +0200

    yasen commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+def odd_numbers(n):
+    return [x for x in range(n) if x % 2 == 1]
+
+
+def print_menu():
+    print("1. Odd numbers")
+
+
+def main():
+    print_menu()
+    function = int(input("Your choice: "))
+    maxNum = int(input("Enter max number: "))
+    if function == 1:
+        print(str(odd_numbers(maxNum))[1:-1])
+    else:
+        print("Invalid choice")
+
+
+if __name__ == '__main__':
+    main()
 
