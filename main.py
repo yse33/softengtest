4f848b3b52d919814f96f87820272ad3be58df25
Yasen Angelov <39673185+yse33@users.noreply.github.com>
2024-03-21 21:46:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-<<<<<<< HEAD
 def even_numbers(n):
     return [x for x in range(n) if x % 2 == 0]
 
 
-def print_menu():
-    print("2. Even numbers")
-=======
 def odd_numbers(n):
     return [x for x in range(n) if x % 2 == 1]
 
 
 def print_menu():
     print("1. Odd numbers")
->>>>>>> 0d38311998c231b4d53889071f30cb78871c2d88
+    print("2. Even numbers")
 
 
 def main():
     print_menu()
     function = int(input("Your choice: "))
     maxNum = int(input("Enter max number: "))
-<<<<<<< HEAD
     if function == 2:
         print(str(even_numbers(maxNum))[1:-1])
-=======
     if function == 1:
         print(str(odd_numbers(maxNum))[1:-1])
->>>>>>> 0d38311998c231b4d53889071f30cb78871c2d88
     else:
         print("Invalid choice")
 
